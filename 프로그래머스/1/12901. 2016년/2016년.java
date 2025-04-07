784be5c651ad4614f038727fbc86a8bedac6ea80
thezz9 <thezz9@naver.com>
2025-04-07 09:14:00 +0900

    [level 1] Title: 2016년, Time: 1.43 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+class Solution {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        return date.getDayOfWeek().toString().substring(0, 3);
+    }
+}
