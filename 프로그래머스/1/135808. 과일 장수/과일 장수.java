ff18b7087c0b39360a2ab8b85f5ed6f2c6be969f
thezz9 <thezz9@naver.com>
2025-04-09 09:39:14 +0900

    [level 1] Title: 과일 장수, Time: 51.91 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        Arrays.sort(score);
+
+        int answer = 0;
+
+        for (int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
+        }
+
+        return answer;
+    }
+}
