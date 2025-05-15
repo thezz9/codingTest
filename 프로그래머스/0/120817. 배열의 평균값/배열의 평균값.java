812fa469cd9347b63183b801c1e010fadcd19797
Taeik Kim <thezz9@naver.com>
2025-05-16 00:47:06 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,11 +1,9 @@
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        double sum = 0;
-        for (int i : numbers) {
-            sum += i;
-            answer = sum / numbers.length;
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
         }
-        return answer;
+        return (double) sum / numbers.length;
     }
 }
