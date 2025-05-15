203b09fb7f91b479b127f128da430ffa1f944728
Taeik Kim <thezz9@naver.com>
2025-05-16 00:41:06 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer;
         if (num1 == num2) {
-            answer = 1;
-        } else {
-            answer = -1;
-        }
-        return answer;
+            return 1;
+        } 
+        
+        return -1;
     }
 }
