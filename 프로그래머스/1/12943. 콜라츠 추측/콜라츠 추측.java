df85021c732480a95cebd50f16de7764a0d1da44
Taeik Kim <thezz9@naver.com>
2025-05-18 01:53:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,21 +1,14 @@
 class Solution {
-    public long solution(long num) {
-        if (num == 1) {
-            return 0;
-        }
+    public int solution(long num) {
         int count = 0;
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-            } else {
-                num = num * 3 + 1;
 
-            }
+        if (num == 1) return 0;
+
+        while (num > 1) {
+            num = (num % 2 == 0) ? num / 2 : num * 3 + 1;
             count++;
-            if (count >= 500) {
-                return -1;
-            }
         }
-        return count;
+
+        return (count > 500) ? -1 : count;
     }
 }
