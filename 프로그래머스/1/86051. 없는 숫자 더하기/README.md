81f5af56c2725d6e6d0296472ae890cb18d15d6e
Taeik Kim <thezz9@naver.com>
2025-05-24 00:38:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 4.23 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.03 ms
+메모리: 74.7 MB, 시간: 4.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 11일 21:36:36
+2025년 05월 24일 00:38:13
 
 ### 문제 설명
 
--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,16 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] numbers) {
-       int answer = 0;
-       for (int i = 0 ; i <= 9 ; i++) {
-           boolean exists = false;
-           for (int j : numbers) {
-               if (j == i) {
-                   exists = true;
-                   break;
-               }
-           }
-           if (!exists) answer += i;
-       }
-       return answer;
+        return IntStream.rangeClosed(0, 9)
+                .filter(i -> Arrays.stream(numbers).noneMatch(n -> n == i))
+                .sum();
     }
 }
