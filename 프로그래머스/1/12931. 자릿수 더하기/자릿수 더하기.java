9bb8b6490963ac7e824cdeefd4c1b7cebd5b807a
Taeik Kim <thezz9@naver.com>
2025-05-25 05:15:06 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int result = 0;
         while (n > 0) {
-            answer += n % 10;
+            result += n % 10;
             n /= 10;
         }
-
-        return answer;
+        return result;
     }
 }
