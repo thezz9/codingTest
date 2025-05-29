c157dad0675b5eb5a3eeabbf7d64a9b4ef920189
Taeik Kim <thezz9@naver.com>
2025-05-29 09:43:20 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,10 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        while (n != 0) {
-            sb.append(n % 3);
-            n = n / 3;
-        }
-        return Integer.parseInt(String.valueOf(sb), 3);
-    }
+	public int solution(int n) {
+		StringBuilder sb = new StringBuilder();
+		while (n != 0) {
+			sb.append(n % 3);
+			n /= 3;
+		}
+		return Integer.parseInt(sb.toString(), 3);
+	}
 }
