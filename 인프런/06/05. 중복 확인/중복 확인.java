3be9fa7d1c7bf97e63fe0d8791fff02cce4765ad
thezz9 <thezz9@naver.com>
2025-08-11 19:46:43 +0900

    인프런 6-5

---
+++ 인프런/06/05. 중복 확인/중복 확인.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (!set.add(num)) {
+                System.out.println("D");
+                return;
+            }
+        }
+
+        System.out.println("U");
+    }
+}
