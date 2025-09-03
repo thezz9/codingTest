11d99ee5fd79782e0f8c613c4a88ef36ae62bba0
Taeik Kim <thezz9@naver.com>
2025-09-03 15:21:46 +0900

    [Bronze III] Title: 오븐 시계, Time: 120 ms, Memory: 16056 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int add = Integer.parseInt(br.readLine());
+
+        int time = h * 60 + m + add;
+        if (time >= 1440) time -= 24 * 60;
+
+        System.out.println(time / 60 + " " + time % 60);
+    }
+}
