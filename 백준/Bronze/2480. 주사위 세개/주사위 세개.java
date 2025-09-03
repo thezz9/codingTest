247b7d99f9e69cf5cd46c9aedaae499f5183a15b
Taeik Kim <thezz9@naver.com>
2025-09-03 15:38:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int result;
+        if (a == b && b == c) {
+            result = 10000 + a * 1000;
+        } else if (a == b || a == c) {
+            result = 1000 + a * 100;
+        } else if (b == c) {
+            result = 1000 + b * 100;
+        } else {
+            result = Math.max(a, Math.max(b, c)) * 100;
+        }
+
+        System.out.println(result);
+    }
+}
