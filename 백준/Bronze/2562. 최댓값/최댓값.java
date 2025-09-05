1d3e95fe7143b3e43b8230f01f182efe6b2abbfc
Taeik Kim <thezz9@naver.com>
2025-09-06 03:50:39 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0, index = 0;
+        for (int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > max) {
+                max = num;
+                index = i;
+            }
+        }
+
+        System.out.printf("%d\n%d", max, index);
+    }
+}
