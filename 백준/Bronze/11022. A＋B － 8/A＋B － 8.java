ba5d238314af34701bca9220891a5e8b28efec50
Taeik Kim <thezz9@naver.com>
2025-09-06 01:44:04 +0900

    [Bronze V] Title: A+B - 8, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append("Case #")
+                .append(i)
+                .append(": ")
+                .append(a)
+                .append(" + ")
+                .append(b)
+                .append(" = ")
+                .append(a + b)
+                .append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
