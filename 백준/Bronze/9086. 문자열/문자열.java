bd9e899847d22ef7f2ac6b0bae875b6ab0968be9
Taeik Kim <thezz9@naver.com>
2025-09-07 00:31:43 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            sb.append(str.charAt(0)).append(str.charAt(str.length() - 1)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
