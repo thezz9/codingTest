eb7d34f42b6735203fd5ef490cfc715e268113ac
Taeik Kim <thezz9@naver.com>
2025-09-15 22:01:09 +0900

    [Bronze II] Title: 대표값2, Time: 124 ms, Memory: 16040 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        bw.write(sum / 5 + "\n" + arr[2]);
+        bw.flush();
+    }
+}
