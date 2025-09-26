bba923f9b9ed9b56494557af368f89dd9b9720cf
Taeik Kim <thezz9@naver.com>
2025-09-26 17:12:19 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 196 ms, Memory: 18904 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            count = 0;
+            sb.append(isPalindrome(s)).append(' ').append(count).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+
+    public static int recursion(String s, int l, int r) {
+        count++;
+        if (l >= r) {
+            return 1;
+        } else if (s.charAt(l) != s.charAt(r)) {
+            return 0;
+        } else {
+            return recursion(s, l + 1, r - 1);
+        }
+    }
+}
