bfe1a9d4a336c48e6414b5a30e67f358b820998f
Taeik Kim <thezz9@naver.com>
2025-09-27 20:02:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 420 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int code1 = fib(n);
+        int code2 = n - 2;
+
+        bw.write(code1 + " " + code2);
+        bw.flush();
+    }
+
+    static int fib(int n) {
+        if (n == 1 || n == 2) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
