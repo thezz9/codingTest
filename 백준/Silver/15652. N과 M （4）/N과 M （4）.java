6fc789c99ea467ccceac84ebf70cc97ac89dc89f
Taeik Kim <thezz9@naver.com>
2025-10-09 22:38:14 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        backtrack(1, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static void backtrack(int start, int depth) {
+        if (depth == m) {
+            for (int x : arr) sb.append(x).append(' ');
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            backtrack(i, depth + 1);
+        }
+    }
+}
