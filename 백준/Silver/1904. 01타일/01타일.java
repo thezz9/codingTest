5f708941811f59ba8e4e1ff2724ccbcc8ccb24e8
Taeik Kim <thezz9@naver.com>
2025-10-23 22:20:37 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            bw.write("1");
+            bw.flush();
+            return;
+        }
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
+        }
+
+        bw.write(String.valueOf(arr[n]));
+        bw.flush();
+    }
+}
